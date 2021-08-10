0b8a323217fafbadf45bb2a7db30db0ae3b4cc04
Jayalaxmipj <pjayalaxmij@gmail.com>
2021-08-10 17:06:06 +0530

    first commit

---
+++ lib/screens/components/body.dart
@@ -0,0 +1,95 @@
+import 'package:ecommerce/components/default_button.dart';
+
+import 'package:ecommerce/constants.dart';
+import 'package:ecommerce/screens/components/splash_content.dart';
+import 'package:ecommerce/size_config.dart';
+import 'package:flutter/material.dart';
+
+class Body extends StatefulWidget {
+  const Body({Key? key}) : super(key: key);
+
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  int currentPage = 0;
+  List<Map<String, String>> splashData = [
+    {
+      "text": "Welcome to FlipKart,Let's shop!",
+      "image": "assets/images/splash_1.png"
+    },
+    {
+      "text": "we help people connect with store\n around India",
+      "image": "assets/images/splash_2.png"
+    },
+    {
+      "text": "we show the easy way to shop.\n Just stay at home with us",
+      "image": "assets/images/splash_3.png"
+    }
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SizedBox(
+        width: double.infinity,
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 3,
+              child: PageView.builder(
+                onPageChanged: (value) {
+                  setState(() {
+                    currentPage = value;
+                  });
+                },
+                itemCount: splashData.length,
+                itemBuilder: (context, index) => SplashContent(
+                  image: splashData[index]["image"].toString(),
+                  text: splashData[index]['text'].toString(),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: getProportionateScreenWidth(20)),
+                child: Column(
+                  children: <Widget>[
+                    Spacer(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: List.generate(
+                          splashData.length, (index) => buildDot(index: index)),
+                    ),
+                    Spacer(flex: 2),
+                    DefaultButton(
+                      text: "Continue",
+                      press: () {},
+                    ),
+                    Spacer(),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  AnimatedContainer buildDot({required int index}) {
+    return AnimatedContainer(
+      duration: kAnimationDuration,
+      margin: EdgeInsets.only(right: 5),
+      height: 6,
+      width: currentPage == index ? 20 : 6,
+      decoration: BoxDecoration(
+        color: currentPage == index ? kPrimaryColor : Color(0xFFD8D8D8),
+        borderRadius: BorderRadius.circular(3),
+      ),
+    );
+  }
+}
